cba65eaaca65051a0393a84eb488ebf380b59be0
DioMain <dima123.zample@gmail.com>
2022-03-10 19:49:50 +0300

    tittle page complete

---
+++ js/MainPageScript.js
@@ -0,0 +1,11 @@
+function HL1_href(){
+    window.location.href = 'https://www.youtube.com';
+}
+
+function HL2_href(){
+    window.location.href = 'https://www.youtube.com';
+}
+
+function HLALYX_href(){
+    window.location.href = 'https://www.youtube.com';
+}
