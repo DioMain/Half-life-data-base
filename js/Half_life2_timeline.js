f06559661b2f4565daebb35529b40290a3e6fcb0
DioMain <dima123.zample@gmail.com>
2022-05-08 15:03:24 +0300

    HL2_beta

--- js/Half_life2_timeline.js
+++ js/Half_life2_timeline.js
@@ -7,7 +7,7 @@ function sellect(value) {
             Dsc_title.innerHTML = "Half-life 2 beta";
             Dsc_title.style = "color: #632b00";
 
-            Dsc_info.innerHTML = "Ранняя версия игры";  
+            Dsc_info.innerHTML = "Half-Life 2 Beta, также известная как Half-Life 2 Alpha, Half-Life 2 Leak, Beta или утечка, — это общее название, которое относится к Half-Life 2 на интересных этапах его разработки, начиная с 1999 года. до его выпуска в 2004 году.";
             break;
         case 2:
             Dsc_title.innerHTML = "Half-life 2";
@@ -19,18 +19,18 @@ function sellect(value) {
             Dsc_title.innerHTML = "Half-life 2: ep 1";
             Dsc_title.style = "color: #c49a56";
 
-            Dsc_info.innerHTML = "Небольшой эпизод";  
-             break;
+            Dsc_info.innerHTML = "Небольшой эпизод";
+            break;
         case 4:
             Dsc_title.innerHTML = "Half-life 2: ep 2";
             Dsc_title.style = "color: #bdac91";
 
-            Dsc_info.innerHTML = "Завершающий эпизод";  
+            Dsc_info.innerHTML = "Завершающий эпизод";
             break;
         default:
             Dsc_title.innerHTML = "Выберете время...";
             Dsc_title.style = "color: white";
 
-            Dsc_info.innerHTML = "";  
+            Dsc_info.innerHTML = "";
     }
 }
